93e9fae1abaebaad2451b5c78989465c9b430ab6
zhev <wzw@meitu.com>
2018-07-23 21:06:00 +0800

    切屏动画

--- src/js/home.js
+++ src/js/home.js
@@ -4,12 +4,23 @@ import "fullpage.js/dist/jquery.fullpage.css";
 $(document).ready(function() {
     $('#fullpage').fullpage({
         navigation: true, // 是否显示项目导航
-        navigationPosition: "right", // 项目导航的位置，可选 left 或 right
+        navigationPosition: "left", // 项目导航的位置，可选 left 或 right
         navigationColor: '#fff',
         sectionsColor: ['#262431', '#262431', '#262431', '#262431', '#262431', '#2c2d39'],
-        lazyLoading: true
+        lazyLoading: true,
+        afterLoad: function(anchor, index) {
+
+        },
+        onLeave: function(index,nextIndex) {
+            if (index === 1 && nextIndex === 2) {
+                $('#splitPoint').removeClass('backOne').addClass('start');
+            }
+            if (index === 2 && nextIndex === 1) {
+                $('#splitPoint').removeClass('start').addClass('backOne');
+            }
+        }
     });
     $('#profilePhoto').bind('webkitAnimationEnd', function() {
         $(this).addClass('inPosition').unbind('webkitAnimationEnd');
-    })
+    });
 });
